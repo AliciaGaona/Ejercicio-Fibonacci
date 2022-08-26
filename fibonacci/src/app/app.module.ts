4f52569322ea06b1bf8bc40d1b66410d17d787f5
Diti Devs <aligaovela@gmail.com>
2022-08-26 01:49:41 -0500

    Agregando componente de vista principal

--- fibonacci/src/app/app.module.ts
+++ fibonacci/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioPrincipalComponent } from './componentes/formulario-principal/formulario-principal.component';
+import { VistaPrincipalComponent } from './componentes/vista-principal/vista-principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioPrincipalComponent
+    FormularioPrincipalComponent,
+    VistaPrincipalComponent
   ],
   imports: [
     BrowserModule,
