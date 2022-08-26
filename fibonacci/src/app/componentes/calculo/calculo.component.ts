94bf0453f2ba090ee6dfe1772ba60b29c02d4383
Diti Devs <aligaovela@gmail.com>
2022-08-26 04:00:48 -0500

    AgCorrigiendo método fibonacci

--- fibonacci/src/app/componentes/calculo/calculo.component.ts
+++ fibonacci/src/app/componentes/calculo/calculo.component.ts
@@ -20,7 +20,7 @@ export class CalculoComponent implements OnInit {
     const numeroPrimo=this.getNumberFibonacci()
     // const numeroFibonacci=1
     // const numeroTriangular=3
-    // const numeroPrimo=2 
+    // const numeroPrimo=2
     const operacion1=numeroFibonacci*n/numeroTriangular*n
     const operacionSerie= operacion1*numeroPrimo
     // console.log(n,numeroFibonacci,numeroTriangular,numeroPrimo)
@@ -28,8 +28,19 @@ export class CalculoComponent implements OnInit {
   }
 
   getNumberFibonacci(){
-    const numeroFibonacci=1
-    return numeroFibonacci
+    const cantidad=this.num//cantidad de numeros triangular
+    let j = 1 
+    let numeroFibonacci = 1;
+    
+    const numerosFibonacciArray=[];
+        for (let i = 1; i <= cantidad; i++)
+        {
+            j = j + 1;
+            numeroFibonacci = numeroFibonacci + j;
+            numerosFibonacciArray.push(numeroFibonacci);
+        }
+    //const numeroFibonacci=1
+    return numerosFibonacciArray[0]//primer número
   }
 
   getNumberTriangular(){
@@ -37,12 +48,12 @@ export class CalculoComponent implements OnInit {
     let j = 1, numeroTriangular = 1;
     const numerosTrianguloArray=[];
         for (let i = 1; i <= cantidad; i++)
-        {        
-            j = j + 1; 
-            numeroTriangular = numeroTriangular + j; 
+        {
+            j = j + 1;
+            numeroTriangular = numeroTriangular + j;
             numerosTrianguloArray.push(numeroTriangular);
         }
-    //const numeroTriangular=3 
+    //const numeroTriangular=3
     return numerosTrianguloArray[1] //se pasa 2do número de array
   }
 
@@ -53,7 +64,7 @@ export class CalculoComponent implements OnInit {
   for ( let j = 2;j < cantidad; j++) { //números primos comienzan a partir de dos
     if (this.validaPrimo(j)) {
       numerosPrimosArray.push(j);//generando array con números primos
-    }    
+    }
   }
   //const numeroPrimo=2
     return numerosPrimosArray[0]//forzando a que regrese el primer número del array=>2
@@ -63,11 +74,11 @@ export class CalculoComponent implements OnInit {
     for (var i = 2; i < num; i++) { //números primos comienzan a partir de dos
       if (num % i === 0) {
         return false;
-      } 
+      }
     }
     return num !== 1;
   }
- 
+
 
 
 }
